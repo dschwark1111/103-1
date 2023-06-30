cd89a3420a3ad0aa7339d691e4761f104e4c8244
dschwark1111 <135233905+dschwark1111@users.noreply.github.com>
2023-06-29 19:03:09 -0500

    updates

--- To do App/todo.js
+++ To do App/todo.js
@@ -1,9 +1,10 @@
 var tasks = ["Clean Chicken Coop", "Do coding Homework", "Make grocery list" ,"Laundry", "Clean office"]
 function registerTask(){
     var task = document.getElementById("txttask").value;
-    tasks.push(task);
+    tasks.push(tasks);
+    //document.write(`<p> ${i+1}. ${tasks[i]}</p>}`)
 }
 
-for(i=0;i<tasks.length;i++){
-    document.write(`<p> ${i+1}. ${tasks[i]} </p>`);
-}
+//for(i=0;i<tasks.length;i++){
+   // document.write(`<p> ${i+1}. ${tasks[i]} </p>`);
+//}
